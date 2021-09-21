e92a554d54a964e8ebd7239b174c7dbe7954b241
Theadejong <theadejong85@gmail.com>
2021-09-21 17:12:03 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contactsArray from "./contacts.json";
+import ContactList from "./components/ContactList";
+import Menu from "./components/Menu";
+
+const firstContacts = contactsArray.slice(0, 5);
 
 function App() {
+
+  const [contacts, setContacts] = useState(firstContacts);
+
+  function handleDelete(contactId){
+    let contactsToDelete = [...contacts]
+    const filteredContacts = contactsToDelete.filter((contact) => {return contact.id !== contactId;})
+    setContacts(filteredContacts)
+  }
+
+  function sortByName () {
+    let sortedContacts = [...contacts];
+    sortedContacts.sort((a,b) => {
+      if (a.name > b.name) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+
+    setContacts(sortedContacts)
+    
+  }
+
+  function sortByPopularity () {
+    let sortedContacts = [...contacts];
+    sortedContacts.sort((a, b) => b.popularity - a.popularity);
+    setContacts(sortedContacts)
+  }
+
+  function handleRandom() {
+    // always
+    const addContactArray = [...contacts];
+
+    function compareContacts (element, array) {
+        for (let i = 0; i < array.length; i++) {
+            if (element.id === array[i].id) {
+              return false;
+            }
+        }
+        return true;
+    }
+    
+    const filteredContacts =  contactsArray.filter(contact => compareContacts(contact, addContactArray))
+    console.log(filteredContacts)
+    const randomNumber = Math.floor(Math.random()*filteredContacts.length)
+    console.log(randomNumber)
+    const randomContact = filteredContacts[randomNumber];
+    console.log(randomContact)
+    addContactArray.unshift(randomContact)
+    setContacts(addContactArray)
+    console.log(addContactArray)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu handleRandom={handleRandom} sortByName={sortByName} sortByPopularity={sortByPopularity}/>
+      <ContactList contacts={contacts} handleDelete={handleDelete}/>
     </div>
   );
 }
